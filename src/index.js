fec5bf9e08ca53a8d9f43272b78a94f4f06d4080
Thang Vo <thang.vo@taplifetechnology.com>
2019-05-21 13:11:03 +0700

    test 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static(path.join(__dirname, './src/public')));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Node.js & Express' });
