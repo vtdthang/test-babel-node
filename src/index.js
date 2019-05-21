38d48456bb3b8ed89c8c1f72d885df62a74a284a
Thang Vo <thang.vo@taplifetechnology.com>
2019-05-21 14:39:02 +0700

    change some text

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express();
 //app.use(express.static(path.join(__dirname, './src/public')));
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to Node.js & Express' });
+  res.status(200).json({ message: 'Khoa pede' });
 });
 app.listen(process.env.PORT || 3000, () => console.log("Listening to port 3000"));
